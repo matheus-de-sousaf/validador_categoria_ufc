62697de4a2daca919641b657ad0398cb561e4227
Matheus de Sousa <matheussfwp@gmail.com>
2023-08-24 21:11:39 -0300

    Commit_02

--- scripts/script.js
+++ scripts/script.js
@@ -1,27 +1,54 @@
 
 
-function Calcular(){
+function Calcular() {
     let pesoInput = document.querySelector('#pesoInput').value
     let resultado = document.querySelector('#resultado')
     let categoria = ''
-
-    if(pesoInput < 50){
-        categoria = 'De nenhuma categoria. Aumente o seu peso!'
-    }else if(pesoInput <= 57){
+    if (pesoInput == '' || pesoInput == 0) {
+        alert("Insira um número válido")
+    } else if (pesoInput < 50) {
+        resultado.innerHTML = `Você não é de nenhuma categoria`
+    } else if (pesoInput <= 57) {
         categoria = 'Peso Mosca!'
-    }else if(pesoInput <= 61){
+        resultado.innerHTML = `Seu peso é ${pesoInput}kg, você é da categoria: ${categoria}`
+    } else if (pesoInput <= 61) {
         categoria = 'Peso Galo!'
-    }else if(pesoInput <= 66){
-        categoria = 'Peso Pena!';
-    }else if(pesoInput <= 70){
+        resultado.innerHTML = `Seu peso é ${pesoInput}kg, você é da categoria: ${categoria}`
+    } else if (pesoInput <= 66) {
+        categoria = 'Peso Pena!'
+        resultado.innerHTML = `Seu peso é ${pesoInput}kg, você é da categoria: ${categoria}`
+    } else if (pesoInput <= 70) {
         categoria = 'Peso Leve!'
-    }else if(pesoInput <= 84){
+        resultado.innerHTML = `Seu peso é ${pesoInput}kg, você é da categoria: ${categoria}`
+    } else if (pesoInput <= 84) {
         categoria = 'Peso Médio!'
-    }else if(pesoInput > 84){
+        resultado.innerHTML = `Seu peso é ${pesoInput}kg, você é da categoria: ${categoria}`
+    } else if (pesoInput > 84) {
         categoria = 'Peso Pesado!'
+        resultado.innerHTML = `Seu peso é ${pesoInput}kg, você é da categoria: ${categoria}`
     }
 
     console.log(categoria)
-    resultado.innerHTML = `Seu peso é ${pesoInput}kg, você é da categoria: ${categoria}`
 
+
+}
+
+
+function delDiv() {
+    let container = document.querySelector('#container')
+    let resultado = document.querySelector('#resultado')
+    let pesoInput = document.querySelector('#pesoInput')
+    container.style.display = 'none'
+    title.style.display = 'flex'
+
+    pesoInput.value = `0`
+    resultado.innerHTML = ``
+
+}
+
+function startDiv() {
+    let title = document.querySelector('#title')
+    let container = document.querySelector('#container')
+    title.style.display = 'none'
+    container.style.display = 'flex'
 }
